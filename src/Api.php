3982d2b45fa52952723caac1e9849d00c4a1092f
lizhi <zhudaidai>
2020-03-07 19:48:11 +0800

    删除无用控制器

--- src/Api.php
+++ src/Api.php
@@ -39,23 +39,11 @@ class Api
     {
         $attributes = [
             'prefix' => config('api.route.prefix'),
-//            'middleware' => config('api.route.middleware'),
         ];
         $authController = config('api.auth.controller', AuthController::class);
 
         app('router')->group($attributes, function ($router) use ($authController) {
-
-            $router->namespace('\SwiftApi\Controllers')->group(function ($router) {
-                $router->get('database_tool/show_table', 'DatabaseToolController@showTable')->name('api.database_tool.show_table');
-                $router->get('database_tool/get_table_columns/{table_name}', 'DatabaseToolController@getTableColumns')->name('api.database_tool.get_table_columns');
-                $router->post('helper/scaffold', 'HelperController@postScaffold')->name('api.helper.post_scaffold');
-                $router->get('helper/scaffold', 'HelperController@getScaffold')->name('api.helper.get_scaffold');
-                $router->post('helper/hand_of_god', 'HelperController@handOfGod')->name('api.helper.hand_of_god');
-            });
-
-            $router->get('auth/ding_talk_login', $authController . '@getDingTalkLogin')->name('api.ding_talk_login');
-            $router->get('auth/callback_ding_talk_login', $authController . '@callbackDingTalkLogin')->name('api.callback_ding_talk_login');
-            $router->get('auth/logout', $authController . '@logout')->name('api.logout');
+            $router->post('auth/logout', $authController . '@logout')->name('api.logout');
             $router->post('auth/login', $authController . '@login')->name('api.login');
         });
 
