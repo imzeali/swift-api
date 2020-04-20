010950481f6782175695a522dc54e336a686efe1
lizhi <zealiemai@gmail.com>
2020-04-20 23:58:41 +0800

    refactor(流水号特性): 重写了流水号使用Cache缓存

--- src/Traits/WithSerialNumber.php
+++ src/Traits/WithSerialNumber.php
@@ -13,42 +13,57 @@ use Illuminate\Support\Facades\Cache;
 
 trait WithSerialNumber
 {
+
+
     protected static function bootWithSerialNumber()
     {
         static::creating(function ($model) {
-
-            $model->number = self::generateNumber();
+            Cache::lock($model->getKeyLockName())->get(function () use ($model) {
+                $model->number = $model->generateNumber($model->getSerialNumberKey());
+            });
         });
     }
 
-    public static function generateNumber($prefix)
+    public function getKeyLockName()
     {
-        $key = "ORDER_NUMBER_COUNT:{$prefix}:";
-        return Cache::lock($key, 10)->block(10, function () use ($key) {
-            if (is_null(Cache::get($key))) {
-                $number_count = self::resetCount($key);
-            } else {
-                $number_count = Cache::increment($key);
-            }
-            return date('Ymd') . $number_count . str_pad($number_count, 4, '0', STR_PAD_LEFT);
-        });
+        return "{$this->getSerialNumberKey()}:LOCK";
+    }
 
+    public function getSerialNumberKey()
+    {
+        return "SERIA_NUMBER_COUNT:{$this->getNumberPrefix()}:";
 
     }
 
-    public static function resetCount($key)
+    public function generateNumber($key)
+    {
+        if (is_null(Cache::get($key))) {
+            $number_count = $this->resetCount($key);
+        } else {
+            $number_count = Cache::increment($key);
+        }
+        return date('Ymd') . $number_count . str_pad($number_count, 4, '0', STR_PAD_LEFT);
+    }
+
+    public function resetCount($key)
     {
         $today_start_data = date("Y-m-d 00:00:00");
         $today_end_data = date("Y-m-d 23:59:59");
         $today_end_str = strtotime($today_end_data);
         $expire_time = $today_end_str - time();
-        $max_count = (int)substr(self::whereBetween('created_at', [$today_start_data, $today_end_data])->lockForUpdate()->max('number'), -4);
+        $max_count = (int)substr($this->whereBetween('created_at', [$today_start_data, $today_end_data])->lockForUpdate()->max('number'), -4);
         Cache::set($key, $max_count, $expire_time);
+
         return $max_count;
     }
 
-    public function getNumberAttribute()
+    public function getNumberPrefix()
     {
-        return $this->number_prefix ? $this->number_prefix.$this->number : "NO{$this->number}";
+        return 'NO';
+    }
+
+    public function getNumberAttribute($value)
+    {
+        return $this->getNumberPrefix() . $value;
     }
 }
