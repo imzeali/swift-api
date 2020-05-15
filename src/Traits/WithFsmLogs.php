0a43ec3b1d27f11a022a9d38fbe50aeffd13caae
lizhi <zealiemai@gmail.com>
2020-05-15 23:32:02 +0800

    up

--- src/Traits/WithFsmLogs.php
+++ src/Traits/WithFsmLogs.php
@@ -4,13 +4,12 @@
 namespace SwiftApi\Traits;
 
 
-
 trait WithFsmLogs
 {
 
     public function fsm_logs()
     {
-        return $this->morphMany(config('api.database.finite_state_machine_log_model'), 'fsm_logs_able');
+        return $this->morphMany(config('api.database.finite_state_machine_log_model'), 'fsm_logs_able')->orderBy('id', 'desc');
     }
 
     public function operation($transition)
@@ -30,7 +29,13 @@ trait WithFsmLogs
     public function operating_time($transition)
     {
         $logs = $this->fsm_logs()->where('transition', '=', $transition)->orderBy('id', 'desc')->first();
+
+        if (is_null($logs)) {
+            return null;
+        }
+
         return $logs->created_at;
+
     }
 
 }
